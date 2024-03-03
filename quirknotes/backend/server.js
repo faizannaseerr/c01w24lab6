9305b2a9e557df0a2506422e1555f6eb310e5a74
faizannaseer <f.naseer@mail.utoronto.ca>
2024-03-03 16:51:51 -0500

    final edits

--- quirknotes/backend/server.js
+++ quirknotes/backend/server.js
@@ -99,7 +99,6 @@ app.delete("/deleteNote/:noteId", express.json(), async (req, res) => {
     });
 
     if (data.deletedCount === 0) {
-      console.log("HELLLOO");
       return res
         .status(404)
         .json({ error: "Unable to find note with given ID." });
